daccec46a37b815dc75a5006b372c0675df74816
kevinart-maker <kevinodeyemi76@gmail.com>
2025-05-09 21:09:45 +0100

    adjusted footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
 <footer className="flex flex-col gap-15 px-10 py-15">
   <div className="w-full h-[1px] bg-[var(--primary-color)]"></div>
-      <div className="flex flex-col md:flex-row gap-10 items-center md:items-start justify-between px-10">
+      <div className="flex flex-col md:flex-row gap-10 items-center md:items-start justify-between md:px-10">
         <h1 className="text-4xl font-bold self-center text-[var(--primary-color)]">Golf le Fleur</h1>
   
         <div className="flex flex-col items-center gap-4 text-sm">
